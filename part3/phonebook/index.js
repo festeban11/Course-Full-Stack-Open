145109bac096707bfad9a3305748556ed0f11ec3
Fernando Ibacache <fnandoesteban@gmail.com>
2024-12-07 18:30:37 -0300

    Exercices 3.9 - 3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+
+
+app.use(cors())
 
 app.use(express.json());
 
@@ -10,7 +14,7 @@ morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(
     morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
-
+app.use(express.static('dist'))
   
 
 let persons = [
