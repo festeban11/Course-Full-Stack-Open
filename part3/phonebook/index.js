9020ba2f852467e7e342efb080857878606e2867
Fernando Ibacache <fnandoesteban@gmail.com>
2024-12-14 16:33:01 -0300

    Exercices 3.15 - 3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -38,17 +38,25 @@ app.get('/api/info', (request, response) => {
     })
 })
 
-app.get('/api/person/:id', (request, response) => {
-    Person.findById(request.params.id).then(result => {
-        response.json(result)
-    }) 
-})
+app.get("/api/persons/:id", (request, response, next) => {
+    Person.findById(request.params.id)
+      .then((person) => {
+        if (person) {
+          response.json(person);
+        } else {
+          response.status(404).end();
+        }
+      })
+      .catch((error) => next(error));
+  });
 
-app.delete('/api/person/:id', (request, response) => {
-    Person.findByIdAndDelete(request.params.id).then(result => {
-        response.json(result)
-    })
-})
+  app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(() => {
+        response.status(204).end();
+      })
+      .catch((error) => next(error));
+  });
 
 app.post('/api/persons', (req, res) => {
     const body = req.body;
@@ -70,6 +78,38 @@ app.post('/api/persons', (req, res) => {
             res.status(500).json({ error: 'Failed to save the contact' });
         });
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body;
+    
+    Person.findByIdAndUpdate(
+        request.params.id,
+        { name: body.name, number: body.number },
+        { new: true }
+      )
+        .then((updatedPerson) => {
+          response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
+    })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" });
+  };
+  
+  app.use(unknownEndpoint);
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+
+app.use(errorHandler)
 
 const PORT = 3001
 app.listen(PORT, () => {
